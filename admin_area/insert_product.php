f2f87fc5966751adac5a89ce2b187999419b623f
Harmun ROSSOUW <hrossouw@c2r5s1.wethinkcode.co.za>
2018-10-07 10:48:09 +0200

    add product page works

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -22,12 +22,12 @@
 
 		<table align="center" width="700" border="2" bgcolor="whitesmoke">
 			<tr align="center">
-				<td colspan="7"><h2>Insert New Post Here</h2></td>
+				<td colspan="7"><h2>Insert New Products</h2></td>
 			</tr>
 
 			<tr>
 				<td align="right"><b>Product Title:</b></td>
-				<td><input type="text" name="product_title" /></td>
+				<td><input type="text" name="product_title" size="60" required/></td>
 			<tr>
 				<td align="right"><b>Product Category:</b></td>
 				<td>
@@ -72,16 +72,16 @@
 				</td>
 			<tr>
 				<td align="right"><b>Product Image:</b></td>
-				<td><input type="file" name="product_image" /></td>
+				<td><input type="file" name="product_image" required/></td>
 			<tr>
 				<td align="right"><b>Product Price:</b></td>
-				<td><input type="text" name="product_price" /></td>
+				<td><input type="text" name="product_price" required/></td>
 			<tr>
 				<td align="right"><b>Product Description:</b></td>
 				<td><textarea name="product_desc" cols="20" rows="10" ></textarea></td>
 			<tr>
 				<td align="right"><b>Product Keywords:</b></td>
-				<td><input type="text" name="product_keywords" /></td>
+				<td><input type="text" name="product_keywords" size="60" required/></td>
 			<tr>
 
 			<tr align="center">
@@ -92,3 +92,32 @@
 
 </body>
 </html>
+
+<!-- Insertingdata into database -->
+<?php
+
+if (isset($_POST['insert_post']))
+{
+	$product_title = $_POST['product_title'];
+	$product_cat = $_POST['product_cat'];
+	$product_brand = $_POST['product_brand'];
+	$product_price = $_POST['product_price'];
+	$product_desc = $_POST['product_desc'];
+	$product_keywords = $_POST['product_keywords'];
+
+	$product_image = $_FILES['product_image']['name'];
+	$product_image_tmp = $_FILES['product_image']['tmp_name'];
+
+	move_uploaded_file($product_image_tmp,"product_images/$product_image");
+
+	$insert_product = "insert into products (product_cat, product_title, product_price, product_brand, product_desc, product_image, product_keywords) values ('$product_cat','$product_title', '$product_price', '$product_brand', '$product_desc', '$product_image', '$product_keywords')";
+
+	$insert_pro = mysqli_query($con, $insert_product);
+
+	if($insert_pro){
+
+		echo "<script>alert('Product has been added!')</script>";
+		echo "<script>window.open('insert_product.php','_self')</script>";
+	}
+}
+?>
