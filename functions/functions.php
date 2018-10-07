9e654d1e967d89ec94b5c49b71cbcf5a6cc89d68
Harmun ROSSOUW <hrossouw@c2r5s1.wethinkcode.co.za>
2018-10-07 13:28:39 +0200

    all products page

--- functions/functions.php
+++ functions/functions.php
@@ -17,7 +17,7 @@ function getCats(){
 		$cat_id = $row_cats['cat_id'];
 		$cat_title = $row_cats['cat_title'];
 
-		echo "<li><a href='#'>$cat_title</a></li>";
+		echo "<li><a href='index.php?cat=$cat_id'>$cat_title</a></li>";
 	}
 }
 
@@ -36,38 +36,154 @@ function getBrands(){
 		$brand_id = $row_brands['brand_id'];
 		$brand_title = $row_brands['brand_title'];
 
-		echo "<li><a href='#'>$brand_title</a></li>";
+		echo "<li><a href='index.php?brand=$brand_id'>$brand_title</a></li>";
 	}
 }
 
 function getPro(){
-	global $con;
 
-	$get_pro = "select * from products order by RAND() LIMIT 0,6";
+	if(!isset($_GET['cat'])){
+		if(!isset($_GET['brand'])){
 
-	$run_pro = mysqli_query($con, $get_pro);
+		global $con;
 
-	while($row_pro=mysqli_fetch_array(($run_pro))){
-		$pro_id = $row_pro['product_id'];
-		$pro_cat = $row_pro['product_cat'];
-		$pro_brand = $row_pro['product_brand'];
-		$pro_title = $row_pro['product_title'];
-		$pro_price = $row_pro['product_price'];
-		$pro_image = $row_pro['product_image'];
+		$get_pro = "select * from products order by RAND() LIMIT 0,6";
 
-		echo "
-				<div id='single_product'>
-					<h3>$pro_title</h3>
-					<img src='admin_area/product_images/$pro_image' width='180' height='180' />
+		$run_pro = mysqli_query($con, $get_pro);
 
-					<p><b>GP $pro_price</b></p>
+		while($row_pro=mysqli_fetch_array(($run_pro))){
+			$pro_id = $row_pro['product_id'];
+			$pro_cat = $row_pro['product_cat'];
+			$pro_brand = $row_pro['product_brand'];
+			$pro_title = $row_pro['product_title'];
+			$pro_price = $row_pro['product_price'];
+			$pro_image = $row_pro['product_image'];
 
-					<a href='details.php?pro_id=$pro_id' style='float:left;'>Details</a>
+			echo "
+					<div id='single_product'>
+						<h3>$pro_title</h3>
+						<img src='admin_area/product_images/$pro_image' width='180' height='180' />
 
-					<a href='index.php?pro_id=$pro_id'><button style='float:right'>Add to Cart</button></a>
-				</div>
-		";
+						<p><b>GP $pro_price</b></p>
+
+						<a href='details.php?pro_id=$pro_id' style='float:left;'>Details</a>
+
+						<a href='index.php?pro_id=$pro_id'><button style='float:right'>Add to Cart</button></a>
+					</div>
+			";
+		}
 	}
 }
+}
+
+function getCatPro(){
+
+	if(isset($_GET['cat'])){
+		
+		$cat_id = $_GET['cat'];
+
+	global $con; 
+	
+	$get_cat_pro = "select * from products where product_cat='$cat_id'";
+
+	$run_cat_pro = mysqli_query($con, $get_cat_pro); 
+	
+	$count_cats = mysqli_num_rows($run_cat_pro);
+	
+	if($count_cats==0){
+	
+	echo "<h2 style='padding:20px;'>We're out of stock in this category. Blame the goblins.</h2>";
+	
+	}
+	
+	while($row_cat_pro=mysqli_fetch_array($run_cat_pro)){
+	
+		$pro_id = $row_cat_pro['product_id'];
+		$pro_cat = $row_cat_pro['product_cat'];
+		$pro_brand = $row_cat_pro['product_brand'];
+		$pro_title = $row_cat_pro['product_title'];
+		$pro_price = $row_cat_pro['product_price'];
+		$pro_image = $row_cat_pro['product_image'];
+	
+		echo "
+				<div id='single_product'>
+				
+					<h3>$pro_title</h3>
+					
+					<img src='admin_area/product_images/$pro_image' width='180' height='180' />
+					
+					<p><b> GP $pro_price </b></p>
+					
+					<a href='details.php?pro_id=$pro_id' style='float:left;'>Details</a>
+					
+					<a href='index.php?pro_id=$pro_id'><button style='float:right'>Add to Cart</button></a>
+				
+				</div>
+		
+		";
+		
+	
+	}
+	
+}
+
+}
+
+
+function getBrandPro(){
+
+	if(isset($_GET['brand'])){
+		
+		$brand_id = $_GET['brand'];
+
+	global $con; 
+	
+	$get_brand_pro = "select * from products where product_brand='$brand_id'";
+
+	$run_brand_pro = mysqli_query($con, $get_brand_pro); 
+	
+	$count_brands = mysqli_num_rows($run_brand_pro);
+	
+	if($count_brands==0){
+	
+	echo "<h2 style='padding:20px;'>We're out of stock for this class. A kobold shall be whipped!</h2>";
+	
+	}
+	
+	while($row_brand_pro=mysqli_fetch_array($run_brand_pro)){
+	
+		$pro_id = $row_brand_pro['product_id'];
+		$pro_cat = $row_brand_pro['product_cat'];
+		$pro_brand = $row_brand_pro['product_brand'];
+		$pro_title = $row_brand_pro['product_title'];
+		$pro_price = $row_brand_pro['product_price'];
+		$pro_image = $row_brand_pro['product_image'];
+	
+		echo "
+				<div id='single_product'>
+				
+					<h3>$pro_title</h3>
+					
+					<img src='admin_area/product_images/$pro_image' width='180' height='180' />
+					
+					<p><b> GP $pro_price </b></p>
+					
+					<a href='details.php?pro_id=$pro_id' style='float:left;'>Details</a>
+					
+					<a href='index.php?pro_id=$pro_id'><button style='float:right'>Add to Cart</button></a>
+				
+				</div>
+		
+		";
+		
+	
+	}
+	
+}
+}
+
+
 
 ?>
+
+
